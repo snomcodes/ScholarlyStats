4cb2627b64fc9101758aca42d3ff101f803f460a
Snom <snomcodes@gmail.com>
2024-07-05 16:25:25 +0100

    infinite scroll functionality

--- src/components/searchBox/index.tsx
+++ src/components/searchBox/index.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState, useCallback } from 'react'
 import axios from 'axios'
 import { useTranslation } from 'react-i18next'
-
-// TODO shorten screen height
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 interface ArxivResult {
   title: string
@@ -15,100 +14,127 @@ export const SearchBox: React.FC = () => {
   const [query, setQuery] = useState('')
   const [results, setResults] = useState<ArxivResult[]>([])
   const [isLoading, setIsLoading] = useState(false)
+  const [page, setPage] = useState(0)
+  const [hasMore, setHasMore] = useState(true)
 
-  const fetchResults = useCallback(async (searchQuery: string) => {
+  const [recentResults, setRecentResults] = useState<ArxivResult[]>([])
+  const [recentPage, setRecentPage] = useState(0)
+  const [hasMoreRecent, setHasMoreRecent] = useState(true)
+  const [showRecent, setShowRecent] = useState(true)
+  const [initialLoadComplete, setInitialLoadComplete] = useState(false)
+
+  const fetchResults = useCallback(async (searchQuery: string, pageNumber: number) => {
     try {
       const response = await axios.get(
-        `http://export.arxiv.org/api/query?search_query=all:${searchQuery}&start=0&max_results=10`,
+        `https://export.arxiv.org/api/query?search_query=all:${encodeURIComponent(searchQuery)}&start=${
+          pageNumber * 10
+        }&max_results=10`,
       )
       const parser = new DOMParser()
       const xml = parser.parseFromString(response.data, 'text/xml')
       const entries = xml.getElementsByTagName('entry')
-      const resultsArray: ArxivResult[] = []
-      for (let i = 0; i < entries.length; i++) {
-        const entry = entries[i]
-        const title = entry.getElementsByTagName('title')[0]?.textContent
-        const link = entry.getElementsByTagName('id')[0]?.textContent
-        const author = entry.getElementsByTagName('author')[0]?.textContent
-
-        if (title && link && author) {
-          resultsArray.push({ title, link, author })
-        }
-      }
-      return resultsArray
+      return Array.from(entries).map((entry) => ({
+        title: entry.getElementsByTagName('title')[0]?.textContent ?? '',
+        link: entry.getElementsByTagName('id')[0]?.textContent ?? '',
+        author: entry.getElementsByTagName('author')[0]?.textContent ?? '',
+      }))
     } catch (error) {
       console.error('Error fetching data:', error)
       return []
     }
   }, [])
 
-  const fetchRecentPapers = useCallback(async () => {
+  const fetchRecentPapers = useCallback(async (pageNumber: number) => {
     try {
-      const ARXIV_API =
-        'http://export.arxiv.org/api/query?search_query=all&sortBy=submittedDate&sortOrder=descending&max_results=10'
-      const response = await axios.get(ARXIV_API)
+      const response = await axios.get(
+        `https://export.arxiv.org/api/query?search_query=all&sortBy=submittedDate&sortOrder=descending&start=${
+          pageNumber * 10
+        }&max_results=10`,
+      )
       const parser = new DOMParser()
       const xml = parser.parseFromString(response.data, 'text/xml')
       if (xml.getElementsByTagName('parsererror').length > 0) {
         throw new Error('Invalid XML')
       }
       const entries = xml.getElementsByTagName('entry')
-      const resultsArray: ArxivResult[] = []
-      for (let i = 0; i < entries.length; i++) {
-        const entry = entries[i]
-        const title = entry.getElementsByTagName('title')[0]?.textContent
-        const link = entry.getElementsByTagName('id')[0]?.textContent
-        const author = entry.getElementsByTagName('author')[0]?.textContent
-
-        if (title && link && author) {
-          resultsArray.push({ title, link, author })
-        }
-      }
-      setResults(resultsArray)
-      console.log('Fetched recent papers:', resultsArray)
+      return Array.from(entries).map((entry) => ({
+        title: entry.getElementsByTagName('title')[0]?.textContent ?? '',
+        link: entry.getElementsByTagName('id')[0]?.textContent ?? '',
+        author: entry.getElementsByTagName('author')[0]?.textContent ?? '',
+      }))
     } catch (error) {
-      console.error('Error fetching recent papers:', error)
+      console.error('Error fetching data:', error)
+      return []
     }
   }, [])
 
-  useEffect(() => {
-    fetchRecentPapers()
-  }, [fetchRecentPapers])
+  const loadMoreRecentResults = useCallback(async () => {
+    if (isLoading || !hasMoreRecent) return
+    setIsLoading(true)
+    const newResults = await fetchRecentPapers(recentPage)
+    setRecentResults((prevResults) => [...prevResults, ...newResults])
+    setIsLoading(false)
+    setHasMoreRecent(newResults.length === 10)
+    setRecentPage((prevPage) => prevPage + 1)
+  }, [fetchRecentPapers, recentPage, isLoading, hasMoreRecent])
+
+  const loadMoreResults = async () => {
+    if (isLoading || !hasMore) return
+    setIsLoading(true)
+    const newResults = await fetchResults(query, page)
+    setResults((prevResults) => [...prevResults, ...newResults])
+    setIsLoading(false)
+    setHasMore(newResults.length === 10)
+    setPage((prevPage) => prevPage + 1)
+  }
 
   const searchArxiv = async () => {
     setIsLoading(true)
+    setPage(1)
     setResults([])
-    const newResults = await fetchResults(query)
+    setHasMore(true)
+    setShowRecent(false)
+    const newResults = await fetchResults(query, 0)
     setResults(newResults)
     setIsLoading(false)
   }
 
+  useEffect(() => {
+    if (!initialLoadComplete) {
+      loadMoreRecentResults().then(() => setInitialLoadComplete(true))
+    }
+  }, [loadMoreRecentResults, initialLoadComplete])
+
   return (
-    <div className="flex min-h-screen flex-col bg-neutral-950 p-2">
-      <div className="my-16 rounded-md bg-neutral-800 py-6 shadow-md">
+    <div className="flex h-screen flex-col bg-neutral-950 p-2">
+      <div className="mb-4 mt-16 rounded-md bg-neutral-800 py-6 shadow-md">
         <div className="container mx-auto flex flex-col items-center justify-center gap-2 sm:flex-row">
           <input
             type="text"
             value={query}
             onChange={(e) => setQuery(e.target.value)}
-            placeholder="Enter search query"
+            placeholder={t('Enter search query')}
             className="w-full flex-grow rounded-md border border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-blue-500 sm:w-auto"
           />
           <button
-            className="w-full rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 sm:w-auto"
+            className="w-full rounded-md bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 sm:w-auto"
             onClick={searchArxiv}
-            disabled={isLoading}
+            disabled={isLoading || query.trim() === ''}
           >
-            {isLoading ? 'Loading...' : 'Search'}
+            {t('Search')}
           </button>
         </div>
       </div>
-      <div
-        className="mt-[-50px] h-screen flex-grow overflow-y-auto rounded-md bg-neutral-800"
-        style={{ scrollBehavior: 'smooth' }}
-      >
-        <div className="container mx-auto px-4 py-4">
-          {results.map((result, index) => (
+      <div id="scrollableDiv" className="flex-grow overflow-y-auto rounded-md bg-neutral-800">
+        <InfiniteScroll
+          dataLength={showRecent ? recentResults.length : results.length}
+          next={showRecent ? loadMoreRecentResults : loadMoreResults}
+          hasMore={(showRecent ? hasMoreRecent : hasMore) && !isLoading}
+          scrollableTarget="scrollableDiv"
+          loader={<h4 className="text-center text-white">{t('Loading...')}</h4>}
+          className="container mx-auto px-4 py-4"
+        >
+          {(showRecent ? recentResults : results).map((result, index) => (
             <div key={index} className="mb-4 rounded-md bg-neutral-900 p-4 shadow-md">
               <a href={result.link} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">
                 {result.title}
@@ -116,7 +142,7 @@ export const SearchBox: React.FC = () => {
               <p className="mt-2 text-sm text-gray-400">{result.author}</p>
             </div>
           ))}
-        </div>
+        </InfiniteScroll>
       </div>
     </div>
   )
